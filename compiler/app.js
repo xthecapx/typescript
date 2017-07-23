3b621431f76e7883a0a7ec11c86e1a1c565542a1
The Cap <xthecapx@gmail.com>
2017-07-23 10:12:56 -0500

    Added compilar section

---
+++ compiler/app.js
@@ -0,0 +1,13 @@
+var myName = "Max";
+var myAge = 27;
+var anything;
+anything = 12;
+//myName = 30;
+function controlMe(isTrue) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    return result;
+}
+//# sourceMappingURL=app.js.map
---
+++ compiler/app.ts
@@ -0,0 +1,16 @@
+let myName: string = "Max";
+let myAge: number = 27;
+let anything;
+anything = 12;
+
+//myName = 30;
+
+function controlMe (isTrue: boolean, somethingElse: boolean) {
+  let result: number;
+  if (isTrue) {
+    result = 12;
+  } else {
+    result = 13;
+  }
+  return result;
+}
