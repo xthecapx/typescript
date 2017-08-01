2ffcdcc6abccacf9cd4c6fff5b4ddcdba98e3c01
The Cap <xthecapx@gmail.com>
2017-08-01 10:41:25 -0500

    Using third party packages

---
+++ using-jquery/app.ts
@@ -0,0 +1,9 @@
+// *** Method 1 ***
+// npm install -g typings
+// typings install dt~jquery --global --save
+// *** Method 2 ***
+// npm install --save-dev @types/jquery
+
+import "jQuery"
+
+$("#app").css({"background-color": "green"});
